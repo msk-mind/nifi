d8105f13d222866cfdfd22f0f8ebe7b3cd07ddbf
doori <doori.rose@gmail.com>
2020-08-06 12:57:30 -0400

    refactor spark session to a separate script

---
+++ src/spark_session.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+"""Common spark session"""
+
+def spark_session(spark_master_uri, driver_ip):
+
+	return SparkSession.builder \
+		.appName("dicom-to-delta") \
+		.master(spark_master_uri) \
+		.config("spark.jars.packages", "io.delta:delta-core_2.12:0.7.0") \
+		.config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.HDFSLogStore") \
+	    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+	    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+		.config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
+		.config("spark.driver.port", "8001") \
+		.config("spark.blockManager.port", "8002") \
+		.config("spark.driver.host", driver_ip) \
+		.config("spark.driver.bindAddress", "0.0.0.0") \
+		.getOrCreate()
